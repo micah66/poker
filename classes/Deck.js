0cfc40b823f84e8d3d83cf52e9ddd7cc4281b10a
micah66 <micahgordon66@gmail.com>
2021-04-02 10:51:19 +0300

    add card visualization and shuffle button

--- classes/Deck.js
+++ classes/Deck.js
@@ -10,6 +10,12 @@ class Deck {
     }
     this.cards = cards
   }
+
+  print() {
+    for (const card of this.cards) {
+      console.log(card.toString())
+    }
+  }
 }
 
 class StandardDeck extends Deck {
@@ -17,6 +23,17 @@ class StandardDeck extends Deck {
     super(['Clubs', 'Diamonds', 'Hearts', 'Spades'], [
       'Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King'
     ])
+
+    const cardColorMap = {
+      clubs: 'green',
+      diamonds: 'blue',
+      hearts: 'red',
+      spades: 'black'
+    }
+
+    for (const card of this.cards) {
+      card.color = cardColorMap[card.suite.toLowerCase()]
+    }
   }
 }
 
--- site/index.js
+++ site/index.js
@@ -1,3 +1,5 @@
+import { PokerDealer } from '../classes/Dealer'
+
 const root = document.getElementById('root')
 root.style.margin = '0'
 root.style.backgroundColor = 'lightgrey'
@@ -13,3 +15,34 @@ header.style.textAlign = 'center'
 header.style.fontSize = ''
 
 root.appendChild(header)
+
+const shuffleBtn = document.createElement('button')
+
+shuffleBtn.innerHTML = 'Shuffle'
+root.appendChild(shuffleBtn)
+shuffleBtn.id = 'shuffleBtn'
+
+const dealer = new PokerDealer()
+shuffleBtn.onclick = () => {
+  dealer.shuffle()
+  //dealer.deck.print()
+  renderDeck()
+}
+
+function renderDeck() {
+  let deckDiv = document.getElementById('deck')
+  if (deckDiv) deckDiv.innerHTML = ''
+  else deckDiv = document.createElement('div')
+  deckDiv.id = 'deck'
+  root.appendChild(deckDiv)
+  for (const card of dealer.deck.cards) {
+    const cardDiv = document.createElement('span')
+    cardDiv.classList.add('card')
+    cardDiv.innerHTML = card.toString()
+    cardDiv.style.marginRight = '10px'
+    cardDiv.style.color = card.color
+    deckDiv.appendChild(cardDiv)
+  }
+}
+
+renderDeck()
