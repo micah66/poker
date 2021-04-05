a9161c093fae74fa47f50da5ee2f75b1337aba4d
micah66 <micahgordon66@gmail.com>
2021-04-05 08:10:45 +0300

    imp card functions in deck rather than dealer

--- classes/Dealer.js
+++ classes/Dealer.js
@@ -1,4 +1,4 @@
-import { StandardDeck } from './Deck'
+import { StandardDeck } from './deck'
 
 class Dealer {
   constructor(deck) {
@@ -6,30 +6,7 @@ class Dealer {
   }
 
   shuffle() {
-    const plusMinus = (Math.random() > 0.5) ? 1 : -1
-    const cut = Math.floor(Math.random() * 5) * plusMinus
-    const leftHand = this.deck.cards.splice(0, (this.deck.cards.length / 2) + cut)
-    const rightHand = this.deck.cards.splice(0, this.deck.cards.length)
-    
-    while (leftHand.length && rightHand.length) {
-      let mult = 0.5
-      let hand = Math.random() > mult ? 'left' : 'right'
-      if (hand === 'left') {
-        this.deck.cards.push(leftHand.shift())
-        mult += 0.1
-      } else {
-        this.deck.cards.push(rightHand.shift())
-        mult -= 0.1
-      }
-    }
-
-    while (leftHand.length) {
-      this.deck.cards.push(leftHand.shift())
-    }
-
-    while (rightHand.length) {
-      this.deck.cards.push(rightHand.shift())
-    }
+    this.deck.shuffle()
   }
 }
 
