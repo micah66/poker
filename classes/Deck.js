853867813607e5af1315d46041e14c600378f1e6
micah66 <micahgordon66@gmail.com>
2021-04-29 00:48:14 +0300

    improved implimentation of deck class

--- classes/Deck.js
+++ classes/Deck.js
@@ -1,115 +1,187 @@
-import { PlayingCard, SUITES, RANKS } from './playing-card'
+import { SUITES, RANKS } from '../constants/playing-card'
 
 class Deck {
-  constructor(cards) {
+  constructor(cards = []) {
     this.cards = cards
   }
 
-  static from(suites, ranks) {
-    const cards = []
-    let i = 0
-    for (const suite of suites) {
-      for (const rank of ranks) {
-        cards.push(new PlayingCard(suite, rank, i++))
-      }
+  validateCanDrawCard(cardPosition) {
+    if ((0 >= cardPosition) || (cardPosition > this.size())) {
+      throw new Error(`RangeError: Cannot remove card from position (${cardPosition})`)
     }
-    return cards
+  }
+
+  validateCanInsertCard(cardPosition) {
+    if ((0 >= cardPosition) || (cardPosition > this.size() + 1)) {
+      throw new Error(`RangeError: Cannot insert card to position ${cardPosition}`)
+    }
+  }
+
+  validateNotEmpty() {
+    if (this.isEmpty()) {
+      throw new Error(`IllegalOperation: Cannot draw card, deck is empty`)
+    }
+  }
+
+  validateNotFull() {
+    if (this.isFull()) {
+      throw new Error(`IllegalOperation: Cannot insert card, deck is full`)
+    }
   }
 
   print() {
-    for (const card of this.cards) {
-      console.log(card.toString())
-    }
+    this.cards.forEach(console.log)
+  }
+
+  drawCard(cardPosition) {
+    this.validateNotEmpty()
+    this.validateCanDrawCard(cardPosition)
+
+    return this.cards.splice(cardPosition - 1, 1)[0]
   }
 
   drawTopCard() {
-    this.validateNotEmpty('drawTopCard')
-
-    const card = this.cards.pop()
-    this.checkIsEmpty()
-
-    return card
+    return this.drawCard(this.size())
   }
   
   drawBottomCard() {
-    this.validateNotEmpty('drawBottomCard')
+    return this.drawCard(1)
+  }
 
-    const card = this.cards.shift()
-    this.checkIsEmpty()
+  insertCard(card, cardPosition) {
+    this.validateNotFull()
+    this.validateCanInsertCard(cardPosition)
 
-    return card
+    const { bottom, top } = this.split(cardPosition - 1)
+    bottom.cards.push(card)
+    
+    this.stack(bottom.stack(top))
   }
 
   insertCardTop(card) {
-    this.cards.push(card)
+    this.insertCard(card, this.size() + 1)
   }
 
   insertCardBottom(card) {
-    this.cards.unshift(card)
+    this.insertCard(card, 1)
   }
 
-  checkIsEmpty() {
-    this.isEmpty = this.cards.length === 0
+  isEmpty() {
+    return this.size() === 0
   }
 
-  validateNotEmpty(action) {
-    if (this.isEmpty) throw new Error(`Illegal action "${action}": Deck is empty`)
+  isFull() {
+    throw new Error(`Missing implementation for this function`)
+  }
+
+  size() {
+    return this.cards.length
+  }
+
+  stack(deck) {
+    this.cards = this.cards.concat(deck.cards)
+
+    return this
+  }
+
+  split(position) {
+    return { 
+      bottom: new Deck(this.cards.splice(0, position)),
+      top: new Deck(this.cards.splice(0, this.cards.length))
+    }
   }
 
   cut() {
-    const [ top, bottom ] = this.split()
-
-    this.cards = bottom.cards.concat(top.cards)
-  }
-
-  split() {
     const plusMinus = (Math.random() > 0.5) ? 1 : -1
     const cut = Math.floor(Math.random() * Math.round(this.cards.length / 10)) * plusMinus
-    const leftHand = this.cards.splice(0, (this.cards.length / 2) + cut)
-    const rightHand = this.cards.splice(0, this.cards.length)
 
-    return [ new Deck(leftHand), new Deck(rightHand) ]
+    const { bottom, top } = this.split((this.cards.length / 2) + cut)
+
+    this.stack(top.stack(bottom))
   }
 
   riffleShuffle() {
-    let [ leftHand, rightHand ] = this.split()
+    const { bottom: leftHand, top: rightHand } = this.split()
 
     let whichHand = 0.5
-    while (!leftHand.isEmpty && !rightHand.isEmpty) {
+    while (!leftHand.isEmpty() && !rightHand.isEmpty()) {
       if (Math.random() > whichHand) {
-        this.cards.push(leftHand.drawBottomCard())
+        this.insertCardTop(leftHand.drawBottomCard())
         whichHand += 0.33
       } else {
-        this.cards.push(rightHand.drawBottomCard())
+        this.insertCardTop(rightHand.drawBottomCard())
         whichHand -= 0.33
       }
     }
 
-    this.cards = this.cards.concat(leftHand.cards)
-    this.cards = this.cards.concat(rightHand.cards)
+    this.stack(rightHand.stack(leftHand))
+  }
+
+  sort(compare) {
+    return this.cards.sort(compare)
   }
 }
 
 class StandardDeck extends Deck {
-  constructor() {
-    super(Deck.from([SUITES.CLUBS, SUITES.DIAMONDS, SUITES.HEARTS, SUITES.SPADES], [
-      RANKS.ACE, RANKS.TWO, RANKS.THREE, RANKS.FOUR, RANKS.FIVE, RANKS.SIX, RANKS.SEVEN, RANKS.EIGHT, RANKS.NINE, RANKS.TEN, RANKS.JACK, RANKS.QUEEN, RANKS.KING
-    ]))
+  static suites = Object.values(SUITES)
+  static ranks = Object.values(RANKS)
+  static cardColorMap = {
+    clubs: 'green',
+    diamonds: 'blue',
+    hearts: 'red',
+    spades: 'black'
+  }
+  static #MAX_SIZE = 52
 
-    const cardColorMap = {
-      clubs: 'green',
-      diamonds: 'blue',
-      hearts: 'red',
-      spades: 'black'
+  static #Card = class {
+    constructor(suite, rank, color, id) {
+      this.suite = suite
+      this.rank = rank
+      this.color = color
+      this.id = id
     }
 
-    for (const card of this.cards) {
-      card.color = cardColorMap[card.suite.toLowerCase()]
+    getSuite() {
+      return this.suite
+    }
+
+    getRank() {
+      return this.rank
+    }
+
+    getId() {
+      return this.id
+    }
+  
+    toString() {
+      return `${this.rank} of ${this.suite}`
     }
   }
 
+  constructor() {
+    super(StandardDeck.suites.reduce((acc, suite, i) => [
+      ...acc, 
+      ...StandardDeck.ranks.map((rank, j) => {
+        return new StandardDeck.#Card(
+          suite,
+          rank,
+          StandardDeck.cardColorMap[suite.toLowerCase()],
+          (i * StandardDeck.ranks.length) + j + 1
+        )
+      })
+    ], []))
+  }
+
   sort() {
-    this.cards.sort((a,b) => a.id - b.id)
+    super.sort((a,b) =>  a.id - b.id)
+  }
+
+  isFull() {
+    return this.size() === StandardDeck.#MAX_SIZE
+  }
+
+  static isCard(card) {
+    return (card instanceof StandardDeck.#Card)
   }
 }
 
