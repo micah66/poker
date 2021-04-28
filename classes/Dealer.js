f4085c020bed8eeaf50636794b25dbf258ece961
micah66 <micahgordon66@gmail.com>
2021-04-29 00:49:46 +0300

    add functionality to the dealer class

--- classes/Dealer.js
+++ classes/Dealer.js
@@ -1,8 +1,13 @@
-import { StandardDeck } from './deck'
+import { Deck, StandardDeck } from './deck'
 
 class Dealer {
   constructor(deck) {
     this.deck = deck
+    this.discardPile = new Deck([])
+  }
+
+  dealCard(player) {
+    player.receiveCard(this.deck.drawTopCard())
   }
 
   shuffle() {
@@ -12,11 +17,41 @@ class Dealer {
   sort() {
     this.deck.sort()
   }
+
+  dealCards(players, amount) {
+    while(amount--) {
+      players.forEach(player => this.dealCard(player))
+    }
+  }
 }
 
 class PokerDealer extends Dealer {
   constructor() {
     super(new StandardDeck())
+  }
+
+  dealCards(players) {
+    super.dealCards(players, 2)
+  }
+
+  burnCard() {
+    this.discardPile.insertCardTop(this.deck.drawTopCard())
+  }
+
+  dealFlop(table) {
+    this.burnCard()
+    for (const i = 0; i < 3; i++) {
+      this.table.playCard(this.deck.drawTopCard())
+    }
+  }
+
+  dealTurn() {
+    this.burnCard()
+    this.table.playCard(this.deck.drawTopCard())
+  }
+
+  dealRiver() {
+    this.dealTurn()
   }
 }
 
